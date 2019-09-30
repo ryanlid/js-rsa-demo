aaabedec0379d7d4cdd4b319d97f4c57e57281ad
ryanlid <ryanlid@live.com>
2019-09-30 15:13:45 +0800

    feat: 🎸 add source code link to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="text-center">在线 rsa 加密解密</div>
+  <div style="text-align:center;margin-top:10px;">
+    <div>
+      <a
+        class="btn"
+        href="https://github.com/ryanlid/js-rsa-demo"
+        target="_blank"
+        >View on Github</a
+      >
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +16,28 @@ export default {
   name: "home"
 };
 </script>
+
+<style scoped>
+.btn {
+  text-align: center;
+  box-sizing: border-box;
+  outline: 0;
+  -webkit-appearance: none;
+  line-height: 20px;
+  display: inline-block;
+  text-decoration: none;
+  vertical-align: middle;
+  cursor: pointer;
+  border: 1px solid #ddd;
+  font-family: Lato, sans-serif;
+  font-size: 18px;
+  padding: 10px 24px;
+  border-radius: 3px;
+  color: #1c86f2;
+  font-weight: 500;
+}
+.btn:hover {
+  color: rgba(28, 134, 242, 0.6);
+  border-color: rgba(28, 134, 242, 0.5);
+}
+</style>
